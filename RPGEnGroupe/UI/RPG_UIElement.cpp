48f08ddd79e7a4d9396bc1e5fb22a4a08f862b87
HugoGlm <hguillaume34@icloud.com>
2023-01-11 13:31:43 +0100

    UIElement

---
+++ RPGEnGroupe/UI/RPG_UIElement.cpp
@@ -0,0 +1,27 @@
+#include "RPG_UIElement.h"
+#include "Manager/UIElementManager.h"
+
+#pragma region constructor
+RPG_UIElement::RPG_UIElement(Window* _owner)
+{
+	owner = _owner;
+	UIElementManager::Instance()->Register(this);
+}
+RPG_UIElement::RPG_UIElement(const RPG_UIElement& _copy)
+{
+	owner = _copy.owner;
+	isActive = _copy.isActive;
+}
+RPG_UIElement::~RPG_UIElement()
+{
+	UIElementManager::Instance()->UnRegister(this);
+}
+#pragma endregion
+
+#pragma region methods
+void RPG_UIElement::OnUpdate() { }
+void RPG_UIElement::SetActive(const bool _status)
+{
+	isActive = _status;
+}
+#pragma endregion
