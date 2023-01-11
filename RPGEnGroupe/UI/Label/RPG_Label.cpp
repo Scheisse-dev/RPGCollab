2369e1847f2ea2c28ac5cc8fa438dc5b392eafdc
HugoGlm <hguillaume34@icloud.com>
2023-01-11 14:07:54 +0100

    label

---
+++ RPGEnGroupe/UI/Label/RPG_Label.cpp
@@ -0,0 +1,50 @@
+#include "RPG_Label.h"
+
+RPG_Label::RPG_Label(Window* _owner, const char* _text)
+{
+}
+
+RPG_Label::RPG_Label(const RPG_Label& _copy)
+{
+}
+
+RPG_Label::~RPG_Label()
+{
+}
+
+void RPG_Label::Free()
+{
+}
+
+void RPG_Label::SetCharacterSize(const int _size)
+{
+}
+
+void RPG_Label::SetColor(const sf::Color _color)
+{
+}
+
+void RPG_Label::SetText(const char* _text)
+{
+}
+
+void RPG_Label::SetPosition(const sf::Vector2f& _position)
+{
+}
+
+void RPG_Label::SetScale(const sf::Vector2f& _scale)
+{
+}
+
+void RPG_Label::Draw(Window* _window)
+{
+}
+
+void RPG_Label::SetOrigin(const sf::Vector2f& _origin)
+{
+}
+
+sf::FloatRect RPG_Label::GetGlobalBounds() const
+{
+    return sf::FloatRect();
+}
