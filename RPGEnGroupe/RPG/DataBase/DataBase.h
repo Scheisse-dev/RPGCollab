f6d99c598ae7675869ca2b92c68831cfa4f7be1c
ExtOnix <73810047+ExtOnix@users.noreply.github.com>
2023-01-11 16:50:52 +0100

    Entity/player

--- RPGEnGroupe/RPG/DataBase/DataBase.h
+++ RPGEnGroupe/RPG/DataBase/DataBase.h
@@ -5,6 +5,11 @@
 
 #define SAMUS "../Donnees/Samus.png"
 
+//Entity
+#define PLAYER_MAX_LIFE 100
+#define MOB_MAX_LIFE 50
+
+
 
 #define LABEL_FONT ""
 
---
+++ RPGEnGroupe/RPG/Entity/RPG_Entity/RPG_Player/RPG_Player.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "../RPG_Entity.h"
+class RPG_Player : public RPG_Entity
+{
+#pragma region f/p
+private:
+
+#pragma endregion f/p
+#pragma region constructor/destructor
+public:
+
+#pragma endregion constructor/destructor
+#pragma region methods
+public:
+
+#pragma endregion methods
+#pragma region override
+public:
+
+#pragma endregion override
+#pragma region operator
+public:
+
+#pragma endregion operator
+};
+
