8ab826f6c3c729f5415e03eefc59d76576bf2c0c
HugoGlm <hguillaume34@icloud.com>
2023-01-11 12:14:03 +0100

    re window

--- RPGEnGroupe/Window/Window.h
+++ RPGEnGroupe/Window/Window.h
@@ -1,24 +1,44 @@
 #pragma once
-#include "../Object/Object.h"
+#include "../../Object/Object.h"
+#include <SFML/Graphics.hpp>
+#include <map>
+
+class BaseMenu;
 
 class Window : public Object
 {
 #pragma region f/p
-private:
-    bool isOpen = false;
-    sf::RenderWindow window = sf::RenderWindow();
+protected:
+	const char* title = "default Window";
+	sf::RenderWindow* window = nullptr;
+	std::map<const char*, BaseMenu*> menus = std::map<const char*, BaseMenu*>();
+protected:
+	bool eventIfNotFocus = false;
 #pragma endregion
 #pragma region constructor
 public:
-    Window();
-    virtual ~Window() override;
+	Window() = default;
+	Window(const char* _title);
+	Window(const Window& _copy) = delete;
+	virtual ~Window();
 #pragma endregion
 #pragma region methods
+private:
+	void Update();
 public:
-    void Open();
-    void Close();
-    bool IsOpen();
-    void OnDraw();
-    void OnUpdate();
+	void Open();
+	void Close();
+	bool IsOpen() const;
+	bool HasFocus() const;
+	void SetFrameLimit(const int _frame);
+	void Draw(sf::Drawable* _drawable);
+	sf::RenderWindow* Renderer() const;
+protected:
+	void CloseAllMenus();
+	void OpenMenu(const char* _name);
+	void RegisterMenu(const char* _name, BaseMenu* _menu);
+	virtual void InitMenus();
+	virtual void OnDraw() = 0;
+	virtual void OnUpdate() = 0;
+	virtual void OnReceiveEvent(const sf::Event& _event);
 #pragma endregion
-};
