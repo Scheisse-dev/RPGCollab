4267aab1451793d81aac03daefc29d06db7b1cdb
HugoGlm <hguillaume34@icloud.com>
2023-01-11 13:58:01 +0100

    Label

---
+++ RPGEnGroupe/UI/Label/RPG_Label.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../RPG_UIElement.h"
+
+class RPG_Label : public RPG_UIElement
+{
+#pragma region f/p
+private:
+	sf::Text* text = nullptr;
+	sf::Font* font = nullptr;
+#pragma endregion f/p
+#pragma region constructor/destructor
+public:
+	RPG_Label(Window* _owner, const char* _text);
+	RPG_Label(const RPG_Label& _copy);
+	~RPG_Label() override;
+#pragma endregion constructor/destructor
+#pragma region methods
+public:
+	void Free();
+	void SetCharacterSize(const int _size);
+	void SetColor(const sf::Color _color);
+	void SetText(const char* _text);
+#pragma endregion methods
+#pragma region override
+public:
+	virtual void SetPosition(const sf::Vector2f& _position) override;
+	virtual void SetScale(const sf::Vector2f& _scale) override;
+	virtual void Draw(Window* _window) override;
+	virtual void SetOrigin(const sf::Vector2f& _origin) override;
+	virtual sf::FloatRect GetGlobalBounds() const override;
+#pragma endregion override
+};
+
