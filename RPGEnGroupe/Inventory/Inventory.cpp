c81bf14454351c82b5fa35abc4eab14f6c5d8d80
ExtOnix <73810047+ExtOnix@users.noreply.github.com>
2023-01-12 15:56:23 +0100

    Inventory

--- RPGEnGroupe/Inventory/Inventory.cpp
+++ RPGEnGroupe/Inventory/Inventory.cpp
@@ -1,1 +1,69 @@
 #include "Inventory.h"
+#include <iostream>
+
+
+Inventory::~Inventory()
+{
+	
+}
+
+void Inventory::RemoveItem(std::string _type, int _number)
+{
+	Gold _goldItem;
+	PotionHeal _healItem;
+	PotionMana _manaItem;
+	if (_type == _healItem.Name() && healPotionCount != 0)
+	{
+		healPotionCount -= _number;
+	}
+	else if (_type == _manaItem.Name() && manaPotionCount != 0)
+	{
+		manaPotionCount -= _number;
+	}
+	else if (_type == _goldItem.Name() && goldCount != 0)
+	{
+		goldCount -= _number;
+	}
+	else
+	{
+		std::cout << "[Inventory] => nom de la ressource demande n'est pas valide ou stock egale a 0 !";
+	}
+}
+
+void Inventory::AddItem(std::string _type, int _number)
+{
+	Gold _goldItem;
+	PotionHeal _healItem;
+	PotionMana _manaItem;
+	if (_type == _healItem.Name())
+	{
+		healPotionCount += _number;
+	}
+	else if (_type == _manaItem.Name())
+	{
+		manaPotionCount += _number;
+	}
+	else if (_type == _goldItem.Name())
+	{
+		goldCount += _number;
+	}
+	else
+	{
+		std::cout << "[Inventory] => nom de la ressource demande n'est pas valide !";
+	}
+}
+
+int Inventory::HealPotionCount() const
+{
+	return healPotionCount;
+}
+
+int Inventory::ManaPotionCount() const
+{
+	return manaPotionCount;
+}
+
+int Inventory::GoldCount() const
+{
+	return goldCount;
+}
--- RPGEnGroupe/Inventory/Inventory.h
+++ RPGEnGroupe/Inventory/Inventory.h
@@ -1,5 +1,44 @@
 #pragma once
-class Inventory
+#include "../Object/Object.h"
+#include "../UI/Image/RPG_Image.h"
+#include "../Item/Potion/Heal/PotionHeal.h"
+#include "../Item/Potion/Mana/PotionMana.h"
+#include "../Item/Gold/Gold.h"
+
+class Inventory : public Object
 {
+#pragma region f/p
+private:
+	int healPotionCount = START_HEALPOTION_COUNT;
+	int manaPotionCount = START_MANAPOTION_COUNT;
+	int goldCount = START_GOLD_COUNT;
+
+#pragma endregion f/p
+#pragma region constructor/destructor
+public:
+	Inventory() = default;
+	~Inventory() override;
+
+#pragma endregion constructor/destructor
+#pragma region methods
+public:
+
+	void RemoveItem(std::string _type, int _number);
+	void AddItem(std::string _type, int _number);
+
+
+	int HealPotionCount() const;
+	int ManaPotionCount() const;
+	int GoldCount() const;
+
+#pragma endregion methods
+#pragma region override
+public:
+
+#pragma endregion override
+#pragma region operator
+public:
+
+#pragma endregion operator
 };
 
