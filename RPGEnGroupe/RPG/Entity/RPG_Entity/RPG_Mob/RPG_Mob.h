54f5132eb2e6a79befa7b439b6490c35a44f6e00
Scheisse <78857441+Scheisse-dev@users.noreply.github.com>
2023-01-13 12:12:32 +0100

    mob class

--- RPGEnGroupe/RPG/Entity/RPG_Entity/RPG_Mob/RPG_Mob.h
+++ RPGEnGroupe/RPG/Entity/RPG_Entity/RPG_Mob/RPG_Mob.h
@@ -1,21 +1,37 @@
 #pragma once
 #include "../RPG_Entity.h"
+
+class Animation;
+
 class RPG_Mob : public RPG_Entity
 {
 #pragma region f/p
 private:
 
+	sf::Clock* patern = nullptr;
+	sf::Clock* speed = nullptr;
+	sf::Sprite* sprite = nullptr; 
+	sf::Texture* texture = nullptr;
+	sf::Vector2f position;
+	float scale = 0; 
+	const char* path = "";
+	const char* name = "";
 #pragma endregion f/p
 #pragma region constructor/destructor
 public:
 	RPG_Mob() = default;
-	RPG_Mob(const int _life, const int _damage, const char* _name);
+	RPG_Mob(const int _life, const int _damage, const char* _name, const char* _path, const float _scale, sf::Sprite* _sprite, sf::Vector2f _position);
 	RPG_Mob(const RPG_Mob& _copy);
 	~RPG_Mob() override;
 #pragma endregion constructor/destructor
 #pragma region methods
 public:
-
+	void SetPosition();
+	void SetScale(); 
+	void SetTexture();
+	void Draw(sf::RenderWindow _owner); 
+	void Move();
+	void SetAnimation(); 
 #pragma endregion methods
 #pragma region override
 public:
@@ -24,11 +40,5 @@ public:
 	virtual std::string Name() const override;
 
 #pragma endregion override
-#pragma region operator
-public:
-
-#pragma endregion operator
-
-	// Inherited via RPG_Entity
 };
 
